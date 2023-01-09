791076184c00cce46487f64d6ebd31840670a897
freetimezp <kraftukrnet@ukr.net>
2023-01-09 22:01:52 +0200

    11. js-services-04-crypto. Scroll reveal animation effect.

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,3 +53,22 @@ const toggleActive = function () {
 }
 
 addEventOnElem(addToFavBtns, "click", toggleActive);
+
+
+/* scroll reveal effect */
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function() {
+    for(let i = 0; i < sections.length; i++) {
+        if(sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
+            sections[i].classList.add("active");
+        }else{
+            sections[i].classList.remove("active");
+        }
+    }
+};
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
+
